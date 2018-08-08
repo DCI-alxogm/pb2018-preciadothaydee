ebcdd9ea89cb257bae52516481eaa0cef54c989f
Haydee Preciado <haydeepreciado99@gmail.com>
2018-08-08 09:37:34 -0500

    Hola como estas

---
+++ semana1/programa_hola.c
@@ -0,0 +1,16 @@
+ /*
+     Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo. Por  ejemplo:
+     Este programa imprime la típica frase “hola mundo a la pantalla”
+     Creado el 06/08/2018 por …Haydee
+    */
+
+#include<stdio.h>
+
+int main ()
+{
+printf("Hola Alma \n");
+printf("Este es el primer programa del curso\n");
+
+}
+
+/*Termine de escribir el programa*/
