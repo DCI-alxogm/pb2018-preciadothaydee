34f05ffd71b63589a597a505e6b74717866b5df8
Preciado Trujillo Haydee <preciadoth2018@dci25-164.ugto.mx>
2018-09-28 16:04:04 -0500

    hola

--- Projecto1/projecto1.c
+++ Projecto1/projecto1.c
@@ -2,7 +2,7 @@
 
 #include<stdio.h>
 #include<math.h>
-
+/*Se declaran las variables*/
 int main(){
 float G=39.47841,h;
 int n=1000,i,o;
@@ -11,14 +11,14 @@ double a[n],b[n],c[n],x[n],y[n],z[n],r;
 FILE *planeta;
 FILE *resultado;
 
-
-printf("Escoge que orbita quieres calcular: \ntierra[1] \nmercurio[2] \nvenus[3]  \nmarte[4] \njupiter[5] \nsaturno[6] \nurano[7] \nneptuno[8] \npluton[9]\n");
+/*Se utiliza un condicional if para saber que orbita se desea calcular*/
+printf("Escoge que orbita quieres calcular: \ntierra[1] \nmercurio[2] \nvenus[3]  \nmarte[4] \njupiter[5] \nsaturno[6] \nurano[7] \nneptuno[8] \n");
 scanf("%i",&o);
 
 if(o==1)
 {
 	planeta=fopen("datos.txt","r"); 
-	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f %i",&a1,&a2,&a3,&a4,&a5,&a6,&h);
+	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f ",&a1,&a2,&a3,&a4,&a5,&a6,&h);
 	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\t %f\t  \n",a1,a2,a3,a4,a5,a6,h);
 	fclose(planeta);
 	
@@ -29,27 +29,27 @@ if(o==1)
 	y[0]=a5;
 	z[0]=a6;
 	resultado=fopen("resultado.txt","w"); 
-		for(i=1;i<1000;i++) 
+		for(i=1;i<n;i++) 
 	{
 	  
     	   
 	    a[i]=a[i-1]+x[i-1]*h; 
 	    b[i]=b[i-1]+y[i-1]*h;
 	    c[i]=c[i-1]+z[i-1]*h;
-  	    r=sqrt(pow(a[i],2)+pow(b[i],2)+pow(c[i],2));	  
+  	    r=sqrt(pow(a[i-1],2)+pow(b[i-1],2)+pow(c[i-1],2));/*Se calcula el radio con el que se trabajara*/	  
 	    x[i]=x[i-1]-h*((G*a[i-1])/pow(r,3));
 	    y[i]=y[i-1]-h*((G*b[i-1])/pow(r,3));
 	    z[i]=z[i-1]-h*((G*c[i-1])/pow(r,3));
 
-	    fprintf(resultado," %lf\t %lf\t %lf\t %lf\t %lf\t %lf\n",a[i],b[i],c[i],x[i],y[i],z[i]);
+	    fprintf(resultado," %lf\t %lf\t %lf\t %lf\t %lf\t %lf\n",a[i],b[i],c[i],x[i],y[i],z[i]);/*Se imprimen los resultados para poder graficar*/
 	}
 	    fclose(resultado);
 }
 
 if(o==2)
 {
-	planeta=fopen("datos.txt","r"); 
-	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f %i",&a1,&a2,&a3,&a4,&a5,&a6,&h);
+	planeta=fopen("mercurio.txt","r"); 
+	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f ",&a1,&a2,&a3,&a4,&a5,&a6,&h);
 	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\t %f\t  \n",a1,a2,a3,a4,a5,a6,h);
 	fclose(planeta);
 	
@@ -60,7 +60,7 @@ if(o==2)
 	y[0]=a5;
 	z[0]=a6;
 	resultado=fopen("resultado.txt","w"); 
-		for(i=1;i<1000;i++) 
+		for(i=1;i<n;i++) 
 	{
 	  
     	   
@@ -79,8 +79,8 @@ if(o==2)
 
 if(o==3)
 {
-	planeta=fopen("datos.txt","r"); 
-	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f %i",&a1,&a2,&a3,&a4,&a5,&a6,&h);
+	planeta=fopen("venus.txt","r"); 
+	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f ",&a1,&a2,&a3,&a4,&a5,&a6,&h);
 	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\t %f\t  \n",a1,a2,a3,a4,a5,a6,h);
 	fclose(planeta);
 	
@@ -91,7 +91,7 @@ if(o==3)
 	y[0]=a5;
 	z[0]=a6;
 	resultado=fopen("resultado.txt","w"); 
-		for(i=1;i<1000;i++) 
+		for(i=1;i<n;i++) 
 	{
 	  
     	   
@@ -109,8 +109,8 @@ if(o==3)
 }
 if(o==4)
 {
-	planeta=fopen("datos.txt","r"); 
-	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f %i",&a1,&a2,&a3,&a4,&a5,&a6,&h);
+	planeta=fopen("marte.txt","r"); 
+	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f ",&a1,&a2,&a3,&a4,&a5,&a6,&h);
 	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\t %f\t  \n",a1,a2,a3,a4,a5,a6,h);
 	fclose(planeta);
 	
@@ -139,8 +139,8 @@ if(o==4)
 }
 if(o==5)
 {
-	planeta=fopen("datos.txt","r"); 
-	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f %i",&a1,&a2,&a3,&a4,&a5,&a6,&h);
+	planeta=fopen("jupiter.txt","r"); 
+	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f ",&a1,&a2,&a3,&a4,&a5,&a6,&h);
 	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\t %f\t  \n",a1,a2,a3,a4,a5,a6,h);
 	fclose(planeta);
 	
@@ -169,8 +169,8 @@ if(o==5)
 }
 if(o==6)
 {
-	planeta=fopen("datos.txt","r"); 
-	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f %i",&a1,&a2,&a3,&a4,&a5,&a6,&h);
+	planeta=fopen("saturno.txt","r"); 
+	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f ",&a1,&a2,&a3,&a4,&a5,&a6,&h);
 	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\t %f\t  \n",a1,a2,a3,a4,a5,a6,h);
 	fclose(planeta);
 	
@@ -197,10 +197,11 @@ if(o==6)
 	}
 	    fclose(resultado);
 }
+
 if(o==7)
 {
-	planeta=fopen("datos.txt","r"); 
-	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f %i",&a1,&a2,&a3,&a4,&a5,&a6,&h);
+	planeta=fopen("urano.txt","r"); 
+	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f ",&a1,&a2,&a3,&a4,&a5,&a6,&h);
 	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\t %f\t  \n",a1,a2,a3,a4,a5,a6,h);
 	fclose(planeta);
 	
@@ -229,38 +230,8 @@ if(o==7)
 }
 if(o==8)
 {
-	planeta=fopen("datos.txt","r"); 
-	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f %i",&a1,&a2,&a3,&a4,&a5,&a6,&h);
-	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\t %f\t  \n",a1,a2,a3,a4,a5,a6,h);
-	fclose(planeta);
-	
-	a[0]=a1; 
-	b[0]=a2;
-	c[0]=a3;
-	x[0]=a4;
-	y[0]=a5;
-	z[0]=a6;
-	resultado=fopen("resultado.txt","w"); 
-		for(i=1;i<1000;i++) 
-	{
-	  
-    	   
-	    a[i]=a[i-1]+x[i-1]*h; 
-	    b[i]=b[i-1]+y[i-1]*h;
-	    c[i]=c[i-1]+z[i-1]*h;
-  	    r=sqrt(pow(a[i],2)+pow(b[i],2)+pow(c[i],2));	  
-	    x[i]=x[i-1]-h*((G*a[i-1])/pow(r,3));
-	    y[i]=y[i-1]-h*((G*b[i-1])/pow(r,3));
-	    z[i]=z[i-1]-h*((G*c[i-1])/pow(r,3));
-
-	    fprintf(resultado," %lf\t %lf\t %lf\t %lf\t %lf\t %lf\n",a[i],b[i],c[i],x[i],y[i],z[i]);
-	}
-	    fclose(resultado);
-}
-if(o==9)
-{
-	planeta=fopen("datos.txt","r"); 
-	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f %i",&a1,&a2,&a3,&a4,&a5,&a6,&h);
+	planeta=fopen("neptuno.txt","r"); 
+	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f ",&a1,&a2,&a3,&a4,&a5,&a6,&h);
 	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\t %f\t  \n",a1,a2,a3,a4,a5,a6,h);
 	fclose(planeta);
 	
@@ -289,5 +260,5 @@ if(o==9)
 }
 
 
-return 0;
+return 0;/*Finaliza en programa*/
 }
