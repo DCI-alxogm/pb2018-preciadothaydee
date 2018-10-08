dcf614a1d564bbe124a6913a126c0f4c88fc08b5
Preciado Trujillo Haydee <haydeepreciado99@gmail.com>
2018-10-08 09:32:24 -0500

    hola

---
+++ semana9/ejercicio1.c
@@ -0,0 +1,19 @@
+/*Este programa encontrara el numero mayor entre una lista de numeros dados en otro archivo*/
+
+#include<stdio.h>
+int main ()
+{
+FILE *fp
+
+int i,*ptr;
+int var[i];
+ptr=&var;
+
+fp=fopen("num_maximo.txt","r");
+
+printf("Introduce el numero de datos que hay en tu archivo: \n");
+scanf("%i",&i);
+
+for (i=0;i<var[i];i++)
+{
+
