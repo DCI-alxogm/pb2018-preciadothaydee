e04165207d43fc2f30f8f23fc624fb5e3013fcb0
preciadothaydee <42146579+preciadothaydee@users.noreply.github.com>
2018-10-29 17:18:09 -0600

    Update projecto2.c

--- projecto2/projecto2.c
+++ projecto2/projecto2.c
@@ -1,35 +1,36 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<stdio.h>//Se incluyen las librerias necesarias
 #include<math.h>
+#include<stdlib.h>
+
 
 
 void funcion(){
-	float epsilon,x=1;
+	float epsilon,x=1;//Se declaran las variables que se utilizaran
 	int N,n=0,i,j,mas;
 	float ti,t2,t3,t4,a,t;
 	char num[25];
 
 
-	FILE *datos;
+	FILE *datos;//Se indica que archivos se utilizaran
 	FILE *resultados;
 	FILE *inicial;	
 
 
-datos=fopen("informacion.txt","r");
-fscanf(first,"%f %f %f %f %f %i",&t1,&t2,&t3,&t4,&epsilon,&n);
+datos=fopen("informacion.txt","r");//Se abre el archivo con la informacion necesaria
+fscanf(first,"%f %f %f %f %f %i",&t1,&t2,&t3,&t4,&epsilon,&n);//Se guardan los datos del documento
 fclose(first);
 
 
 
-double **T = (double**)malloc(n*sizeof(int*));
-	for(i=1;i<=n;i++){
+double **T = (double**)malloc(n*sizeof(int*));//Se reserva el espacio de memoria
+	for(i=1;i<=n;i++){//Pimer ciclo for del archivo
 	T[i]=(double*)malloc(n*sizeof(int));
 }	
 
 
-inicial=fopen("inicial.txt","w");
+inicial=fopen("inicial.txt","w");//Se abre un archivo nuevo
 	for(i=1;i<=n;i++){
-		for(j=1;j<=n;j++){
+		for(j=1;j<=n;j++){//SE usa un condicional 
 			if(i=1)
 			T[i][j]=t1;
 			else if(i=n)
@@ -40,18 +41,18 @@ inicial=fopen("inicial.txt","w");
 			T[i][j]=t4;
 			else
 			T[i][j]=0;
-			fprintf(in,"%lf",T[i][j]);
+			fprintf(inicial,"%lf",T[i][j]);
 			}
-		fprintf(in,"\n");
+		fprintf(inicial,"\n");
 		}
-fclose(in);
+fclose(inicial);
 N=n*n;
 
 
 	while(fabs(x)>=epsilon){	
 	if(plus%25==0){
 		sprintf(num,"%i.ods",n);
-		resultados=fopen("resultados.txt","w");
+		resultados=fopen("resultados.txt","w");//SE imprimen los resultados en el archivo de resulatados
 		}
 
 		for(i=1;i<=n-1;i++)
@@ -87,8 +88,8 @@ N=n*n;
 		if(mas%25==0){
 		N++;
 		}
-	fclose(resultados);
+	fclose(resultados);//Se cierra archivo 
 	}
 
-free(T);
+free(T);//Se libera la memoria
 }
